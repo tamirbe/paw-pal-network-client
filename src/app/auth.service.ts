2c8bcfa914e404b4420b4858fce85460144d7f84
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-07-21 12:54:00 +0300

    update error registration if faild that its will get respons from the server why it failed

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -31,18 +31,13 @@ export class AuthService {
     );
   }
 
-  register(username: string, firstName: string, lastName: string, email: string, password: string, dateOfBirth: string): Observable<boolean> {
+  register(username: string, firstName: string, lastName: string, email: string, password: string, dateOfBirth: string): Observable<string> {
     const url = `${this.apiUrl}/register`;
     return this.http.post<{ message: string }>(url, { username, firstName, lastName, email, password, dateOfBirth }).pipe(
-      map(response => {
-        if (response.message === 'User registered') {
-          return true;
-        }
-        return false;
-      }),
+      map(response => 'User registered'),
       catchError(error => {
         console.error('Registration failed', error);
-        return of(false);
+        return of(error.error);
       })
     );
   }
