33c7ee96f4593b245b71f233ca114c2202a21259
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-08-01 00:25:11 +0300

    update

--- src/app/pages/main/home/home.component.ts
+++ src/app/pages/main/home/home.component.ts
@@ -100,6 +100,7 @@ export class HomeComponent implements OnInit {
   onFileChange(event: any): void {
     const file = event.target.files[0];
     this.selectedFile = file; // שמירת הקובץ במשתנה
+    this.postForm.patchValue({image: file})
   }
 
 
