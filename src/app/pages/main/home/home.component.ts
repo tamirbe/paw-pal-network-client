e2e97c9cd734bf663829e8e9b96fce2f52e90e19
Elad Gelerenter <eladg3@gmail.com>
2024-07-31 19:27:48 +0300

    fix Edit image

--- src/app/pages/main/home/home.component.ts
+++ src/app/pages/main/home/home.component.ts
@@ -99,7 +99,8 @@ export class HomeComponent implements OnInit {
 
   onFileChange(event: any): void {
     const file = event.target.files[0];
-    this.selectedFile = file; // שמירת הקובץ במשתנה
+    this.selectedFile = file;
+    this.postForm.patchValue({image : file}); // שמירת הקובץ במשתנה
   }
 
 
