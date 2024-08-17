3284a596b64bd673115771635bd00d4c8bbee456
Roei01 <107935185+Roei01@users.noreply.github.com>
2024-08-17 22:46:21 +0300

    update interest page

--- src/app/pages/main/interests/interests.component.ts
+++ src/app/pages/main/interests/interests.component.ts
@@ -15,18 +15,48 @@ interface Category {
   interests: Interest[];
 }
 
+interface Post {
+  _id?: string;
+  author: string; // או authorName
+  authorName: string;
+  authorProfileImage: string; 
+  createdAt: Date;
+  description: string;
+  image?: string;
+  likes: any[];
+  shares: any[];
+  liked?: boolean;
+  shared?: boolean;
+  saves: string[];
+  saved: boolean;
+  interests?: Interest;
+}
+
+export interface User {
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  pet: string;
+}
+
 @Component({
   selector: 'app-interests',
   templateUrl: './interests.component.html',
   styleUrls: ['./interests.component.scss']
 })
 export class InterestsComponent implements OnInit {
+  currentUser: string = '';
   interests: Interest[] = [];
+  user?: User | null;
+  posts: Post[] = [];
   popularInterests: Interest[] = [];
   filteredInterests: Interest[] = [];
   followingInterests: Interest[] = [];
   categories: Category[] = [];
   searchQuery: string = '';
+  saveSuccess: boolean = false;
+  unsaveSuccess: boolean = false;
   loading: boolean = false;
   currentSection: string = 'allInterests';
 
@@ -37,6 +67,7 @@ export class InterestsComponent implements OnInit {
   async ngOnInit(): Promise<void> {
     this.loading = true; // Start loading indicator
     this.loadUserFollowingInterests();
+    this.loadFollowedInterestsPosts();
     this.loadInterests();
     this.loadPopularInterests();
     this.loadCategories();
@@ -46,6 +77,94 @@ export class InterestsComponent implements OnInit {
   showSection(section: string): void {
     this.currentSection = section;
   }
+
+  getTextDirection(text: string): string {
+    const isHebrew = /[\u0590-\u05FF]/.test(text);
+    return isHebrew ? 'rtl' : 'ltr';
+  }
+
+  async likePost(post: Post) {
+    try {
+      const token = this.authService.getToken();
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      
+      
+      if (post.liked) {
+        post.liked = false;
+        post.likes = post.likes.filter(like => like !== this.currentUser); // מסיר את המשתמש מרשימת הלייקים
+        } 
+        else {
+        post.liked = true;
+        post.likes.push(this.currentUser);
+      }
+      await firstValueFrom(this.http.post(`${this.apiUrl}/posts/${post._id}/like`, {}, { headers }));
+      this.updateLikes(post);
+
+    } catch (error) {
+      console.error('Error liking/unliking post:', error);
+    }
+  }
+
+  updateLikes(post: Post) {
+    const updatedPostIndex = this.posts.findIndex(p => p._id === post._id);
+    if (updatedPostIndex !== -1) {
+      this.posts[updatedPostIndex] = { ...post };
+    }
+  }
+
+  
+
+  async sharePost(post: Post) {
+    try {
+      const token = this.authService.getToken();
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+        
+      const sharedPost: Post = await firstValueFrom(this.http.post<Post>(`${this.apiUrl}/posts/${post._id}/share`, {}, { headers }));
+
+      this.posts.push(sharedPost); 
+    } catch (error) {
+      console.error('Error sharing/unsharing post:', error);
+    }
+  }
+
+  async savePost(post: Post) {
+    try {
+      const token = this.authService.getToken();
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+
+      if (!post.saves) {
+        post.saves = [];
+      }
+
+      if (post.saved) {
+        post.saved = false;
+        post.saves = post.saves.filter(save => save !== this.currentUser);
+      } else {
+        post.saved = true;
+        post.saves.push(this.currentUser);
+      }
+      await firstValueFrom(this.http.post(`${this.apiUrl}/posts/${post._id}/save`, {}, { headers }));
+      if (post.saved){
+        this.unsaveSuccess = false;
+        this.saveSuccess = true; }
+      else {
+        this.saveSuccess = false; 
+        this.unsaveSuccess = true;
+      }
+    } catch (error) {
+      console.error('Error save/unsave post:', error);
+    }
+  }
+
+  async loadFollowedInterestsPosts() {
+    try {
+      const token = this.authService.getToken();
+      const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+      this.posts = await firstValueFrom(this.http.get<Post[]>(`${this.apiUrl}/interests-posts`, { headers }));
+    } catch (error) {
+      console.error('Error loading posts:', error);
+    }
+  }  
 
   async loadInterests() {
     try {
